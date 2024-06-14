ca02a834827228c1157b15c824955886bf4b7efb
esteban vanegas <esteb097@gmail.com>
2024-06-13 23:44:33 -0500

    registro de blueprint en app.py

--- back/app.py
+++ back/app.py
@@ -1,16 +1,23 @@
-
+from flask import Flask, jsonify
 from flask_cors import CORS
-from flask import Flask
 from config import Config
-from flask_sqlalchemy import SQLAlchemy
+from models import db
+from routes import register_blueprints
 
-
+# Crear instancia de Flask
 app = Flask(__name__)
 app.config.from_object(Config)
-CORS(app) 
 
-db = SQLAlchemy(app)
+# Configurar SQLAlchemy
+db.init_app(app)
 
+# Configurar CORS para permitir todas las rutas desde localhost:3000
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
+
+# Registrar blueprints
+register_blueprints(app)
+
+# Rutas u otros ajustes de la aplicación
 @app.route('/')
 def index():
     return 'Hola, esta es la API para la base de datos de aves.'
